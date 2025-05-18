922c515b8e9e547e73564b5366184836612bb961
satyamupwanshi <125626924+satyamupwanshi@users.noreply.github.com>
2025-05-18 17:20:48 +0530

    Update urls.py

--- dealership/urls.py
+++ dealership/urls.py
@@ -6,10 +6,14 @@ from rest_framework_simplejwt.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Vroomle is live!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # 👈 this handles the `/` path
     path('api/', include('vehicles.urls')),  # ✅ change this line
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
